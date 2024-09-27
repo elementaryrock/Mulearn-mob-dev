40e078dacf1c0d2f6bd84fbd2ab10d63caf17314
Maanas MS <maanasms@yahoo.com>
2024-09-27 12:50:42 +0000

    Written code for finding sum of two nos

---
+++ Tasks/sum.dart
@@ -0,0 +1,6 @@
+import 'dart.io';
+print('Enter the first no');
+var first = int.parse(stdin.readLineSync());
+print('Enter the second no');
+var second = int.parse(stdin.readLineSync());
+print('The sum is ${first + second}');
